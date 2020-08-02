da6e4b90c4c4cca0b45ac8ec689d5cffbd194444
Trinhdvt <trinhvideo123@gmail.com>
2020-08-02 22:27:43 +0700

    controller

---
+++ src/main/java/com/test/controller/web/HomeController.java
@@ -0,0 +1,23 @@
+package com.test.controller.web;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/trang-chu"})
+public class HomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("views/web/home.jsp");
+        rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
