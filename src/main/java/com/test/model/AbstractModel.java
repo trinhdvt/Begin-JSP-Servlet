1bacdbe457ac2389b8020d1c40b84b500ca9db3c
Trinhdvt <trinhvideo123@gmail.com>
2020-08-02 22:28:12 +0700

    Model

---
+++ src/main/java/com/test/model/AbstractModel.java
@@ -0,0 +1,49 @@
+package com.test.model;
+
+public class AbstractModel {
+    protected long id;
+    protected String createdDate;
+    protected String modifiedDate;
+    protected String createdBy;
+    protected String modifiedBy;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+}
