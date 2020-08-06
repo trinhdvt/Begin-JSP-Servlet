af921e160c4fe0fed8b5c6e1b751e12581e65e36
Trinhdvt <trinhvideo123@gmail.com>
2020-08-06 20:00:12 +0700

    debug servlet weld

--- src/main/java/com/test/controller/web/HomeController.java
+++ src/main/java/com/test/controller/web/HomeController.java
@@ -1,5 +1,9 @@
 package com.test.controller.web;
 
+import com.test.model.CategoryModel;
+import com.test.service.ICategoryService;
+
+import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,11 +11,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/trang-chu"})
 public class HomeController extends HttpServlet {
+
+    @Inject
+    ICategoryService categoryService;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<CategoryModel> list = categoryService.findAll();
+
         RequestDispatcher rd = req.getRequestDispatcher("views/web/home.jsp");
         rd.forward(req, resp);
     }
