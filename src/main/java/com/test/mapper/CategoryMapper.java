b0fe1296e69545a7e087e738bff767fbeb90c307
Trinhdvt <trinhvideo123@gmail.com>
2020-08-12 22:23:43 +0700

    implement

--- src/main/java/com/test/mapper/CategoryMapper.java
+++ src/main/java/com/test/mapper/CategoryMapper.java
@@ -3,11 +3,20 @@ package com.test.mapper;
 import com.test.model.CategoryModel;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CategoryMapper implements RowMapper<CategoryModel> {
     @Override
     public CategoryModel mapRow(ResultSet rs) {
-
-        return null;
+        try {
+            CategoryModel category = new CategoryModel();
+            category.setId(rs.getLong("id"));
+            category.setName(rs.getString("name"));
+            category.setCode(rs.getString("code"));
+            return category;
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+            return null;
+        }
     }
 }
