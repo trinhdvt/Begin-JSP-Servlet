8b892704bdacd874339462ac787355199ca8d617
Trinhdvt <trinhvideo123@gmail.com>
2020-09-01 21:44:34 +0700

    update Constant

--- src/main/java/com/test/constant/Constant.java
+++ src/main/java/com/test/constant/Constant.java
@@ -3,5 +3,7 @@ package com.test.constant;
 public class Constant {
     public static final String MODEL = "model";
     public static final String CATEGORY = "category";
-
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+    public static final String USER_MODEL = "USERMODEL";
 }
